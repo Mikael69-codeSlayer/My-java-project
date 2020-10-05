6e4d3865f472ce1e2e75d38671a08fda8f4fd0da
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-10-05 22:38:11 +0200

    Big changes! I added gameStart(), setGameRounds, and gameRestart(). I also added try-catch and while in these methods. So if answer is wrong, the question will repeat. Now I have Start menu. I can choose how many rounds I want to play, and I can close the game.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -4,81 +4,89 @@ package com.company;
 import java.util.*;
 
 public class Game {
-
-    // Variables
-    int gameRounds;
-    int gamePlayer; // Array
-    int currentRound;
+                      // Scanner for input
+    private final Scanner scan = new Scanner(System.in);
 
     // My "start" method
-    public void gameStart() {  // Exception for else statement
-        // Scanner
-        Scanner scan = new Scanner(System.in);
+    protected void gameStart() {
         // Menu:  start - end
-        System.out.println("=== Start Menu ===");
+        System.out.println("===== Start Menu =====");
         System.out.println("1. Start a new game" +
                 "\n2. Exit");
-        int playerAnswer = scan.nextInt();
+            // try catch. If you enter words/letters
+        // It will throw an error message
+        try {
+                // String -> int. Because I want only figures here
+                String playerInput = scan.next();
+                int playerInputInteger = Integer.parseInt(playerInput);
+                // If-else statements
+                if (playerInputInteger == 1) {
 
-        // switch-case
-        switch (playerAnswer) {
-            case 1:
-                if (playerAnswer == 1) { // If player starts a new game
-                    // Rounds
-                    System.out.println("How many rounds do you want to play? (5-30)");
-                    // While loop.
-                    // You can only choose between 5-30
-                    while (true) {
-                        gameRounds = scan.nextInt();  //  scanner
-                        if (gameRounds >= 5 && gameRounds <= 30) {  // 5 to 30
-                            System.out.println("You will play " + gameRounds + " rounds.");
-                            break;  // breaks the loop
-                        } else {
-                            System.out.println("You need to choose between 5 and 30!");
-                        }
+                    setGameRounds(); // Calls setGameRounds-method
 
-                    }
-                    // Ask about players
-                    System.out.println("How many players will play? (1-4): ");
-                    // Another while-loop.
-                    // Max 4 players
-                    while (true) {
-                        gamePlayer = scan.nextInt();  // scanner
-                        if (gamePlayer >= 1 && gamePlayer <= 4) {   // 1 to 4
-                            System.out.println(gamePlayer + " player/s will play the game.");
-                            break;   // breaks the loop
-                        } else if (gamePlayer == 0) {
-                            System.out.println("There can't be 0 players." +
-                                    "\nYou need to have at least 1.");
-                        } else {
-                            System.out.println("Max 4 players!");
-                        }
+                } else if (playerInputInteger == 2) {
 
-                    }
+                    gameRestart();  // Calls gameRestart-method
 
-                    // Ask players names
-                    System.out.println("Write player's name: ");
-
-                    // Call getName()-method from class Player
-                    Player p = new Player(); // Player class
-                    p.getName(); // getName-method with scanner
-                    System.out.println("Player list: " + p.name);  // print player list
-
-                    break;
-
+                } else {
+                    System.out.println("Enter 1 or 2!");
+                    gameStart();
                 }
-                        case 2:
-                            if (playerAnswer == 2) { // exit
-                                System.out.println("... exit");
-                                System.exit(0);
-                                break;
-                            }
-                            // A message if player press wrong button
-                        default:
-                            System.out.println("You need to choose 1 or 2!");
-                    }
+              // throwsn an Exception
+            } catch (Exception e) {
+                System.out.println("No letters / words here!" +
+                        "\nEnter 1 or 2!");
+                gameStart();
+            }
+    }
+
+    // Game Rounds
+    private void setGameRounds() {
+        System.out.println("How many rounds do you want to play? (5-30)");
+       while (true) {
+            try {
+                String gameRounds = scan.next();
+                int gameRoundsInt = Integer.parseInt(gameRounds);
+                if (gameRoundsInt >= 5 && gameRoundsInt <= 30) {
+                    System.out.println("You will play " + gameRoundsInt + " rounds.");
+
+                } else {
+                    System.out.println("You need to choose between 5 and 30!");
+                    setGameRounds();
                 }
+                break;
+
+            } catch (Exception e) {
+                System.out.println("You cannot type letters/words here!" +
+                       " Please, try again. Choose between 5 - 30");
+
+            }
         }
+    }
+
+    // Play again
+    private void gameRestart() {
+        System.out.println("Are you sure that you want to exit? [yes/no]");
+        String playerInput2 = scan.next();
+       // if-statement
+        if (playerInput2.equals("yes")) {
+            System.out.println("You closed the game");
+            System.exit(0);
+
+        } else if (playerInput2.equals("no")) {
+            gameStart();  // Calls gameStart()-method and returns Start Menu
+
+        } else {
+            // Throws an error message if you enter wrong text
+            // or figures
+            System.out.println("Error.... Enter yes or no!");
+            gameRestart(); // Calls gameRestart()-method
+        }
+    }
+
+}
+
+
 
 
 
