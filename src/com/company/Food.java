a7e163b73d1a887aec0d3cc250d28d81962cf226
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-10-14 03:54:19 +0200

    Fixed amount for food. No duplicates.

--- src/com/company/Food.java
+++ src/com/company/Food.java
@@ -5,22 +5,23 @@ import java.util.Arrays;
 public abstract class Food {
 
     static String[] foodTypes = {"CatNoodles", "Fish", "Bananas", "Berries", "Carrot", "Cucumber", "Zucchini",
-            "Melons, Pumpkin, Grass", "Carrots, Apples, SugarBeet",
+            "Melons", "Pumpkin", "Grass", "Pear", "Apples", "SugarBeet",
             "Corn", "Soybeans"};
     static int[] foodPrices = {50, 100, 20, 150, 20, 20, 45,
             35, 40, 20, 20, 35, 10, 15, 20};
+    public int amount = 1; // amount
 
     public String foodName;
     public int aFoodPrice;
 
-    public Food createFood(String foodName) {
+    public Food createFood(String foodName, int amount) {
         var animalFoodNumber = Arrays.asList(foodTypes).indexOf(this.getClass().getSimpleName());
 
         this.aFoodPrice = foodPrices[animalFoodNumber];
         this.foodName = foodName;
+        this.amount = amount;
         return this;
     }
-
 
 }
 //Food classes that extends Food abstract class
@@ -54,7 +55,7 @@ class Pumpkin extends Food {
 class Grass extends Food {
 }
 
-class Carrots extends Food {
+class Pear extends Food {
 }
 
 class Apples extends Food {
