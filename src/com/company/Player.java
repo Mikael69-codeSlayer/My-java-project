617e852ce1e8cd43dbbc91d1850faa1e4e89d89d
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-10-17 16:23:34 +0200

    50% chance to get kittens

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -11,8 +11,8 @@ public class Player {
     public String name;
     public int money = 10000;
     // Own Animals and Food
-     public ArrayList<Animal> ownedAnimals = new ArrayList<>();
-     public ArrayList<Food> ownedFood = new ArrayList<>();
+    public ArrayList<Animal> ownedAnimals = new ArrayList<>();
+    public ArrayList<Food> ownedFood = new ArrayList<>();
 
 
     public Player(String name) {
@@ -21,24 +21,25 @@ public class Player {
 
     public void myAnimals() {
         // animal list
-        for(var animal : ownedAnimals) {
+        for (var animal : ownedAnimals) {
             System.out.println("Animal: " + animal.type
                     + ", Name: " + animal.animalName
                     + ", Gender: " + animal.animalGender
                     + ", Health: " + animal.healthPoints);
 
             // Decreases health
-           if(ownedAnimals.size() > 0) {
-               var randomNumber = (int) ((Math.random() * (31 - 10)) + 10);
-               animal.healthPoints -= randomNumber;
-           }
+            if (ownedAnimals.size() > 0) {
+                var randomNumber = (int) ((Math.random() * (31 - 10)) + 10);
+                animal.healthPoints -= randomNumber;
+            }
         }
         notAlive();
 
 
     }
+
     public void myFood() {
-        for(var food : ownedFood) {
+        for (var food : ownedFood) {
             System.out.println("Food: " + food.foodName + " " + food.amount + "kg");
 
         }
@@ -46,7 +47,7 @@ public class Player {
 
     public void giveFood() {
 
-        if(ownedFood.size() > 0) {
+        if (ownedFood.size() > 0) {
             myAnimals();
 
             System.out.println("Choose animal you want to feed (enter type) :  ");
@@ -55,8 +56,7 @@ public class Player {
             String feed = scan.next();
 
 
-
-            if(feed.equalsIgnoreCase("Cat")) {
+            if (feed.equalsIgnoreCase("Cat")) {
                  /*
         Choose between 2 or more animals
         Only chosen animal's hp can increase
@@ -65,31 +65,31 @@ public class Player {
 
          */
 
-                        System.out.println("Your cats can eat: \"1. Cat Noodles\", \"2. Fish\"," +
-                                " \"3. Bananas\", \"4. Berries\"");
-                        System.out.println("You have: ");
-                        myFood();
+                System.out.println("Your cats can eat: \"1. Cat Noodles\", \"2. Fish\"," +
+                        " \"3. Bananas\", \"4. Berries\"");
+                System.out.println("You have: ");
+                myFood();
 
 
-                        System.out.println("Choose food: ");
-                        int feed2 = scan.nextInt();
+                System.out.println("Choose food: ");
+                int feed2 = scan.nextInt();
 
-                        String[] foodTypes = {"Cat Noodles", "Fish", "Bananas", " Berries"};
-                        var chosenFood = foodTypes[feed2 - 1];
+                String[] foodTypes = {"Cat Noodles", "Fish", "Bananas", " Berries"};
+                var chosenFood = foodTypes[feed2 - 1];
 
-                        var howMuch = howMuchFood();
+                var howMuch = howMuchFood();
 
-                        for (var food : ownedFood) {
-                            if (food.foodName.equals(chosenFood)) {
-                                food.amount -= howMuch;
-                                System.out.println("Food left: " + food.amount + "kg");
+                for (var food : ownedFood) {
+                    if (food.foodName.equals(chosenFood)) {
+                        food.amount -= howMuch;
+                        System.out.println("Food left: " + food.amount + "kg");
 
-                            }
+                    }
 
 
-                        }
+                }
 
-                        // howMuchFood();
+                // howMuchFood();
 
 
 
@@ -105,10 +105,7 @@ public class Player {
                     } */
 
 
-
-            }
-
-           else if(feed.equalsIgnoreCase("Rabbit")) {
+            } else if (feed.equalsIgnoreCase("Rabbit")) {
                 System.out.println("Your rabbit can eat: \"1. Carrot\", \"2. Cucumber\", \"3. Zucchini\"");
                 System.out.println("You have:");
                 myFood();
@@ -128,9 +125,7 @@ public class Player {
                     }
 
                 }
-            }
-
-           else if(feed.equalsIgnoreCase("Horse")) {
+            } else if (feed.equalsIgnoreCase("Horse")) {
                 System.out.println("Your horse can eat: \"1. Melons\", \"2. Pumpkin\", \"3. Grass\"");
                 System.out.println("You have:");
                 myFood();
@@ -149,8 +144,7 @@ public class Player {
 
                     }
                 }
-            }
-           else if(feed.equalsIgnoreCase("Donkey")) {
+            } else if (feed.equalsIgnoreCase("Donkey")) {
                 System.out.println("Your Donkey can eat: \"Pear\", \"Apples\", \"SugarBeet\"");
                 System.out.println("You have:");
                 myFood();
@@ -172,11 +166,10 @@ public class Player {
             }
 
 
-        } else  {
+        } else {
             System.out.println("You have no food!");
             g.mainMenu();
         }
-
 
 
     }
@@ -186,9 +179,9 @@ public class Player {
         System.out.println("How many kg?");
         int giveFood = scan.nextInt();
 
-        for(var food : ownedFood) {
+        for (var food : ownedFood) {
 
-            if(food.amount < giveFood) {
+            if (food.amount < giveFood) {
                 System.out.println("try again");
                 howMuchFood();
 
@@ -198,15 +191,15 @@ public class Player {
                 int hp = giveFood + healthIncrease;
 
                 // 1kg food -> +10 hp
-                for(var animal : ownedAnimals) {
+                for (var animal : ownedAnimals) {
 
                     animal.healthPoints += hp;
                     System.out.println(animal.type
-                                       + " "
-                                       + animal.animalName
-                                       + ", "
-                                       + "Health: "
-                                       + animal.healthPoints);
+                            + " "
+                            + animal.animalName
+                            + ", "
+                            + "Health: "
+                            + animal.healthPoints);
                 }
 
 
@@ -233,10 +226,9 @@ public class Player {
     }
 
 
-
     public void notAlive() {
-        for(var animal : ownedAnimals) {
-            if(animal.healthPoints <= 0) {
+        for (var animal : ownedAnimals) {
+            if (animal.healthPoints <= 0) {
                 ownedAnimals.remove(animal);
                 System.out.println("The " + animal.type + " " + animal.animalName + " died");
 
@@ -244,6 +236,60 @@ public class Player {
 
         }
     }
+
+    public void mateAnimals() {
+
+        if(ownedAnimals.size() < 2) {
+            System.out.println("You need to buy 2 animals [male and female]");
+        }
+
+
+        else {
+            System.out.println("=========================  Your list  ==========================");
+            myAnimals();
+            System.out.println("----------------------------------------------------------------");
+            System.out.println("Which animals do you want to mate?" +
+                    "\n1. Cats" +
+                    "\n2. Rabbits" +
+                    "\n3. Horses" +
+                    "\n4. Donkeys" +
+                    "\n5. Pigs" +
+
+                    "\n\n6. ---> MAIN MENU");
+
+            int mateAnimal = scan.nextInt();
+            if(mateAnimal == 1) {
+                System.out.println("Let's make some kittens!");
+              //  int x = (int) (Math.random() * 50);
+                String answer = scan.next();
+                if(answer.equalsIgnoreCase("YES")) {
+
+                    var random = (int)(Math.random()*(2));
+                    if(random == 0) {
+                        System.out.println("---> no kittens :( <---");
+                        mateAnimals();
+                        // System.out.println("Nice! You got new kitten!");
+                       // ownedAnimals.add(new Cat());
+                    } else {
+                        System.out.println("Nice! You got new kitten!");
+                         ownedAnimals.add(new Cat());
+                    }
+
+                }
+            }
+
+        }
+
+    }
+}
+
+           // (ownedAnimals.size() <= 0) {
+             //   System.out.println("You need to buy animals [2]");
+
+
+
+
+
 
 
 
@@ -271,7 +317,7 @@ public class Player {
 
         }
     } */
-}
+
 
 
 
